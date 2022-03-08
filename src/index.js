1b51137e0653a160cfbd75d1480f1d99dfb10561
Joel Chantrell <joelchantrell@gmail.com>
2022-03-08 19:54:11 +1030

    Added menu page + functions for page switch, add menu items

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import './styles.css';
 import {home} from './home.js';
+import {menu, createNewItem} from './menu.js';
 
 const clearContent = () => {
     document.querySelector('.content').replaceChildren();
+    clearLinks();
 }
 
-clearContent();
+const clearLinks = () => {
+    const links = document.querySelectorAll('.link')
+    links.forEach (link => {
+        link.classList.remove('ul');
+    });
+}
+
+const changePage = (() => {
+    document.body.addEventListener('click', function(event){
+        if(event.target.classList.contains('homeLink')) {
+            clearContent();
+            home();
+        };
+        if(event.target.classList.contains('menuLink') || event.target.classList.contains('menuButton')) {
+            clearContent();
+            menu();   
+        };
+        if(event.target.classList.contains('galleryLink')) {
+            
+        };
+        if(event.target.classList.contains('contactLink')) {
+            
+        };
+
+      });
+})();
+
+home();
---
+++ src/menu.js
@@ -0,0 +1,90 @@
+
+export const createNewItem = (name, price, ingredients) => {
+    let newMenuItem = new MenuItem(name, price, ingredients);
+    items.push(newMenuItem);
+}
+
+export const menu = () => {
+
+    const menu = document.createElement('div');
+    const title = document.createElement('div');
+    const menuItems = document.createElement('div');
+    const menuLink = document.querySelector('.menuLink');
+    menuLink.classList.add('ul')
+    document.querySelector('.content').appendChild(menu);
+    menu.classList.add('menu');
+    menu.appendChild(title);
+    title.classList.add('title');
+    title.textContent = 'Our menu'
+    menu.appendChild(menuItems);
+    menuItems.classList.add('menuItems');
+
+    items.forEach(function(item) {
+        const itemContainer = document.createElement('div');
+        const itemName = document.createElement('div');
+        const middle = document.createElement('div');
+        const price = document.createElement('div');
+        const ingredients = document.createElement('div');
+
+        menuItems.appendChild(itemContainer);
+        itemContainer.classList.add('item');
+        itemContainer.appendChild(itemName);
+        itemName.textContent = item.name;
+        itemContainer.appendChild(middle);
+        middle.classList.add('middle')
+        itemContainer.appendChild(price);
+        price.textContent = item.price;
+        menuItems.appendChild(ingredients);
+        ingredients.textContent = item.ingredients;
+        ingredients.classList.add('ingredients');
+    });
+}
+
+class MenuItem {
+    constructor(name, price, ingredients){
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+}
+
+let items = [
+    {
+        "name": "Antipasto",
+        "price": "30",
+        "ingredients": "ingredient, ingredient, ingredient"
+    },
+    {
+        "name": "Pizza",
+        "price": "20",
+        "ingredients": "ingredient, ingredient, ingredient"
+    },
+    {
+        "name": "Calzone",
+        "price": "20",
+        "ingredients": "ingredient, ingredient, ingredient"
+    },
+    {
+        "name": "Pasta",
+        "price": "25",
+        "ingredients": "ingredient, ingredient, ingredient"
+    },
+    {
+        "name": "Gnocci",
+        "price": "25",
+        "ingredients": "ingredient, ingredient, ingredient"
+    },
+    {
+        "name": "Salad",
+        "price": "15",
+        "ingredients": "ingredient, ingredient, ingredient"
+    },
+    {
+        "name": "Soup",
+        "price": "15",
+        "ingredients": "ingredient, ingredient, ingredient"
+    }
+];
+
+createNewItem('Rice Pudding', '10', 'Ingredient, ingredient, ingredient');
+createNewItem('Bread', '5', 'Ingredient, ingredient, ingredient');
