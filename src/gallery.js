414180045b0e9b2ec83750dc615fb5a6adba87a4
Joel Chantrell <joelchantrell@gmail.com>
2022-03-12 12:44:33 +1030

    Fixed img loading for gh-pages

--- src/gallery.js
+++ src/gallery.js
@@ -4,18 +4,18 @@ export const gallery = () => {
     document.querySelector('.content').appendChild(galleryContainer);    
     
     let src = [
-        "../img/tables-1.png", 
-        "../img/tables-2.png", 
-        "../img/meal-1.png", 
-        "../img/chef-2.png", 
-        "../img/chef-3.png", 
-        "../img/chef-1.png",
-        "../img/tables-1.png", 
-        "../img/tables-2.png", 
-        "../img/meal-1.png", 
-        "../img/chef-2.png", 
-        "../img/chef-3.png", 
-        "../img/chef-1.png"]
+        "./img/tables-1.png", 
+        "./img/tables-2.png", 
+        "./img/meal-1.png", 
+        "./img/chef-2.png", 
+        "./img/chef-3.png", 
+        "./img/chef-1.png",
+        "./img/tables-1.png", 
+        "./img/tables-2.png", 
+        "./img/meal-1.png", 
+        "./img/chef-2.png", 
+        "./img/chef-3.png", 
+        "./img/chef-1.png"]
 
     src.forEach(function(imgsrc) {
         const img = document.createElement('img');
